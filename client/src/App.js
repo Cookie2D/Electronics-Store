66b59830409464a6800092ad95cae8ed7d964627
Ihor Sharov <cookie50ds@gmail.com>
2021-09-23 13:23:53 +0300

    Added banner component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {withRouter, Route, Switch} from "react-router-dom";
+import { Route, Switch} from "react-router-dom";
 import NotFound from "./components/pages/NotFound";
 import Home from "./components/pages/Home";
 
