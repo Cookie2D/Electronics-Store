1dac3dd67a4ce4610a45ed444e809dea682e7098
SERG <NIKOLASALON@GMAIL.COM>
2021-09-25 18:23:43 +0300

    created a cardlist of goods, repaired routing and react context

--- server/router/goods.js
+++ server/router/goods.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controllerGoods = require('../controllers/goods');
 
+
 router.get("/laptops", controllerGoods.getLaptops);
 router.get("/phones", controllerGoods.getPhones);
 router.get("/printers", controllerGoods.getPrinters);
