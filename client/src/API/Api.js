47b492b6bc320aa3a5889d0695b544ef736c32c1
Cookie2D <cookie50ds@gmail.com>
2021-10-03 14:30:30 +0300

    renamed API function name

--- client/src/API/Api.js
+++ client/src/API/Api.js
@@ -2,21 +2,21 @@ import axios from "axios";
 
 const BASE_URL = 'http://localhost:4000/api/';
 
-const GoodsAPI = axios.create({
+const API = axios.create({
   baseURL:BASE_URL,
 })
 
 export function getDataByUrl(url) {
-  return GoodsAPI(url)
-    .then(res => res.data)
+  return API(url)
+    .then(res => res.data);
 }
 
 export function getGoodsCategory(category) {
-  return GoodsAPI(`/categories?item=${category}`)
-    .then(res => res.data)
+  return API(`/categories?item=${category}`)
+    .then(res => res.data);
 }
 
 export function getFilteredGoods(url, cost, amount) {
-  return GoodsAPI(url + `?cost=${cost}&amount=${amount}`)
-    .then(res => res.data)
+  return API(url + `?cost=${cost}&amount=${amount}`)
+    .then(res => res.data);
 }
